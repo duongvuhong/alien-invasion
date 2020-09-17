84d94e5e076f18e668998d267ac0a70d6a918d6b
Vu Hong Duong <duong.vuhong.uet@gmail.com>
2020-09-17 17:51:37 +0900

    Add scoring system

--- alien.py
+++ alien.py
@@ -23,7 +23,11 @@ class Alien(Sprite):
 
     def check_edges(self):
         """Return True if alien is at edge of screen."""
-        return self.rect.left <= 0 or self.rect.right >= self.screen.get_rect().right
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
 
     def update(self):
         """Move the alien right or left."""
--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from pygame.sprite import Group
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
 from ship import Ship
 import game_functions as gf
 
-os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (3200,100)
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (3200, 100)
 
 def run_game():
-    """ main game function"""
+    """ main game function """
     # Initialize pygame, settings, and screen object.
     pygame.init()
     ai_settings = Settings()
@@ -19,8 +21,12 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # Create an instance to store game statistics.
+    # Make the Play button.
+    play_button = Button(screen, "Play")
+
+    # Create an instance to store game statistics, and a scoreboard.
     stats = GameStats(ai_settings)
+    score_board = Scoreboard(ai_settings, screen, stats)
 
     # Make a ship, a group of bullets, and a group of aliens.
     ship = Ship(ai_settings, screen)
@@ -32,14 +38,18 @@ def run_game():
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, score_board, play_button, ship,
+            aliens, bullets)
 
         if stats.game_active:
             ship.update()
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+            gf.update_bullets(ai_settings, screen, stats, score_board, ship, aliens,
+                bullets)
+            gf.update_aliens(ai_settings, screen, stats, score_board, ship, aliens,
+                bullets)
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, score_board, ship, aliens,
+            bullets, play_button)
 
 if __name__ == "__main__":
     run_game()
